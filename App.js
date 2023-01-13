7d63f0d3d1438392dec44b8613e11040320e50df
chinmay021 <chinmaykumar021@gmail.com>
2023-01-13 23:13:40 +0530

    chapter 03 - Laying the Foundation

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React.createElement => Object => HTML(DOM)
+
 const heading = React.createElement(
   "h1",
   {
@@ -24,9 +29,41 @@ const container = React.createElement(
 
 console.log(heading);
 
+//JSX
+
+//we can write in different formats
+const HeaderComponent = function () {
+  return (
+    <div>
+      <h1> Namaste React Functional Component</h1>
+      <h2> This is a h2 tag </h2>
+    </div>
+  );
+};
+
+//using arrow function
+const HeaderComponent2 = () => {
+  return (
+    <div>
+      <h1> Namaste React Functional Component</h1>
+      <h2> This is a h2 tag </h2>
+    </div>
+  );
+};
+
+//using arrow function (more minified version)
+const HeaderComponent3 = () => (
+  <div>
+    <h1> Namaste React Functional Component</h1>
+    <h2> This is a h2 tag </h2>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //passing a react element inside the root
 
 //async defer
-root.render(container);
+
+//root.render(container);
+root.render(<HeaderComponent />);
