02bcdde119df8f53435b0ce0cd73d98f45626410
chinmay021 <chinmaykumar021@gmail.com>
2023-02-16 18:44:41 +0530

    changed netlify.toml

--- src/constants.js
+++ src/constants.js
@@ -17,7 +17,7 @@ export const API_URL =
   CORS_PROXY_URL+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.814794130574803&lng=86.09871324151756&page_type=DESKTOP_WEB_LISTING";
 
 export const API_URL2 =
-  CORS_PROXY_URL+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.351841&lng=85.805154&page_type=DESKTOP_WEB_LISTING";
+  CORS_PROXY_URL+"dapi/restaurants/list/v5?lat=20.351841&lng=85.805154&page_type=DESKTOP_WEB_LISTING";
 
 export const FETCH_MENU_URL =
   CORS_PROXY_URL+"https://www.swiggy.com/dapi/menu/v4/full?lat=20.351841&lng=85.805154&menuId=";
