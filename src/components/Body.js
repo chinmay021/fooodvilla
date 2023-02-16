a7bf1065e71c990ecb58ce62fc12a473938610ad
chinmay021 <chinmaykumar021@gmail.com>
2023-02-16 18:19:00 +0530

    added netlify serverless

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,7 +15,6 @@ const Body = () => {
     try {
       const data = await fetch(API_URL2);
       const json = await data.json();
-
       setAllRestaurants(json?.data?.cards?.[2].data?.data?.cards);
       setfilteredRestaurants(json?.data?.cards?.[2]?.data?.data?.cards);
     } catch (error) {
@@ -65,9 +64,9 @@ const Body = () => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               ></path>
             </svg>
--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,23 @@
+
+
+
 export const IMG_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+ "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const IMG__MENU_ITEM_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
+
+// const CORS_PROXY_URL = "https://cors-anywhere.herokuapp.com/"
+const CORS_PROXY_URL = "/api/"
+
+
+
 export const API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.814794130574803&lng=86.09871324151756&page_type=DESKTOP_WEB_LISTING";
+  CORS_PROXY_URL+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.814794130574803&lng=86.09871324151756&page_type=DESKTOP_WEB_LISTING";
 
 export const API_URL2 =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.351841&lng=85.805154&page_type=DESKTOP_WEB_LISTING";
+  CORS_PROXY_URL+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.351841&lng=85.805154&page_type=DESKTOP_WEB_LISTING";
 
-export const FETCH_MENU_URL = "https://www.swiggy.com/dapi/menu/v4/full?lat=22.814794130574803&lng=86.09871324151756&menuId=";
+export const FETCH_MENU_URL =
+  CORS_PROXY_URL+"https://www.swiggy.com/dapi/menu/v4/full?lat=20.351841&lng=85.805154&menuId=";
